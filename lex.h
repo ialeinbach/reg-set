c4abdd2b1594b1fd583e8df5769dea05c892e01f
Ian Leinbach <ialeinbach@vassar.edu>
2018-08-01 10:20:48 -0400

    add extern to funcs for clarity & add ident lexfn_t

--- lex.h
+++ lex.h
@@ -10,10 +10,13 @@ typedef struct {
 	const char *err;
 } lexer_t;
 
-token_t emit(lexer_t *lexer);
-
 typedef int (*lexfn_t)(lexer_t *lexer);
-int regstr(lexer_t *lexer);
-int number(lexer_t *lexer);
-int wspace(lexer_t *lexer);
+
+extern token_t emit(lexer_t *lexer);
+
+extern int regstr(lexer_t *lexer);
+extern int number(lexer_t *lexer);
+extern int wspace(lexer_t *lexer);
+extern int ident(lexer_t *lexer);
+extern token_t next(lexer_t *lexer);
 

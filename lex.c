7e088fbdf3bd8b14b1520c140d1103b901fd6adb
Ian Leinbach <ialeinbach@vassar.edu>
2018-08-01 10:21:25 -0400

    some general redesigns and improvements

--- lex.c
+++ lex.c
@@ -5,23 +5,27 @@
 token_t emit(lexer_t *lexer) {
 	token_t tok = {
 		.type   = lexer->type,
-		.lexeme = lexer->src - lexer->ahead,
+		.lexeme = lexer->src,
 		.len    = lexer->ahead,
 		.line   = lexer->line
 	};
+	lexer->src += lexer->ahead;
 	return tok;
 }
 
 // lexfn_t
 int wspace(lexer_t *lexer) {
+	char *ch = lexer->src;
+
 	for(int c = 0; ; ++c) {
-		switch(*lexer->src) {
+		switch(*ch++) {
 			case '\n':
 				++lexer->line;
 			case '\t':
 			case ' ':
 				continue;
 			default:
+				lexer->src += c;
 				return c;
 		}
 	}
@@ -29,59 +33,68 @@ int wspace(lexer_t *lexer) {
 
 // lexfn_t
 int number(lexer_t *lexer) {
+	char *ch = lexer->src;
+
+	char curr;
 	for(int c = 0; ; ++c) {
-		switch(*lexer->src) {
-			case '0':
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9':
-				++lexer->src;
-				continue;
-			default:
-				if(c > 0) {
-					lexer->type = NUMBER;
-				}
-				return c;
+		curr = *ch++;
+
+		if(curr < '0' || curr > '9') {
+			if(c > 0) {
+				lexer->type = NUMBER;
+				lexer->ahead += c;
+			}
+			return c;
 		}
 	}
 }
 
 // lexfn_t
 int regstr(lexer_t *lexer) {
-	int c = 0;
-	if(*lexer->src == '@') {
-		++lexer->src;
-		if((c = number(lexer)) == 0) {
-			lexer->type = ERROR;
-			lexer->err = "invalid register name";
-		} else {
-			++c; // account for '@'
-			lexer->type = REGSTR;
-		}
+	if(*lexer->src != '@') {
+		return 0;
 	}
+	int c = 1;
+
+	++lexer->src;
+	c += number(lexer);
+	if(c == 1) { // only '@'
+		lexer->type = ERROR;
+		lexer->err = "invalid register name";
+	} else {
+		lexer->type = REGSTR;
+		lexer->ahead += c;
+	}
+	--lexer->src;
+
 	return c;
 }
 
 // lexfn_t
-int instr(lexer_t *lexer) {
-	return 0;
-	/*
+int ident(lexer_t *lexer) {
+	char *ch = lexer->src;
+
+	char curr;
 	for(int c = 0; ; ++c) {
-		switch(*lexer->src) {
+		if(c >= ident_len_max) {
+			lexer->type = ERROR;
+			lexer->err = "identfier too long";
+			return c;
+		}
+		curr = *ch++;
+		if(curr != '_' && (curr < 'a' || curr > 'z') && (curr < 'A' || curr > 'Z')) {
+			if(c > 0) {
+				lexer->type = lookup(lexer->src, c);
+				lexer->ahead += c;
+			}
+			return c;
 		}
 	}
-	*/
 }
 
 token_t next(lexer_t *lexer) {
 	wspace(lexer);
-	if(!(regstr(lexer) || instr(lexer) || number(lexer))) {
+	if(!regstr(lexer) && !ident(lexer) && !number(lexer)) {
 		if(lexer->type != ERROR) {
 			lexer->type = ERROR;
 			lexer->err = "unexpected character";
@@ -90,6 +103,3 @@ token_t next(lexer_t *lexer) {
 	return emit(lexer);
 }
 
-int main() {
-	printf("hello\n");
-}
